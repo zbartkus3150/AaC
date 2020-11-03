b5c905b36961ab5547173d25b0775e18d7284745
zbartkus <zbartkuss3150@gmail.com>
2020-11-03 19:51:19 +0100

    final vol.1

--- AaCProject/Program.cs
+++ AaCProject/Program.cs
@@ -10,12 +10,38 @@ namespace AaCProject
     {
         public static void Main(string[] args)
         {
+            string line;
+            int option;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Input 0 for text file input or 1 for console input:");
+                    line = Console.ReadLine();
+                }
+                while (!(int.TryParse(line, out option)));
+            }
+            while (option != 0 && option != 1);
+
+            if(option == 0)
+            {
+                FileInput();
+            }
+            else
+            {
+                ConsoleInput();
+            }
+
+        }
+
+        public static void ConsoleInput()
+        {
             List<int> initialSet = new List<int>();
             int r;
             Console.WriteLine("Input weights:");
             string line = Console.ReadLine();
             string[] numbers = line.Split(' ');
-            foreach(string number in numbers)
+            foreach (string number in numbers)
             {
                 int n;
                 if (int.TryParse(number, out n))
@@ -23,36 +49,107 @@ namespace AaCProject
             }
             do
             {
-                Console.WriteLine("Input r:");
+                Console.WriteLine("Input number of partitions:");
                 line = Console.ReadLine();
             }
             while (!(int.TryParse(line, out r)));
 
-            List<List<int>> resultBrute = BruteForce(initialSet, r);
+            List<List<int>> resultBrute = BruteForce(initialSet, r, Fitness);
             List<List<int>> resultHeuristic = Heuristic(initialSet, r);
 
             Console.WriteLine("Brute Force solution:");
             PrintSolution(resultBrute);
-            Console.WriteLine("Initial fitness function value: {0}", Fitness(resultBrute));
+            Console.WriteLine("Initial fitness function value: {0}", Fitness(resultBrute, r));
             Console.WriteLine("Second fitness function value: {0}", MyFitness(resultBrute, r));
             Console.WriteLine();
             Console.WriteLine("Heuristic solution:");
             PrintSolution(resultHeuristic);
-            Console.WriteLine("Initial fitness function value: {0}", Fitness(resultHeuristic));
+            Console.WriteLine("Initial fitness function value: {0}", Fitness(resultHeuristic, r));
             Console.WriteLine("Second fitness function value: {0}", MyFitness(resultHeuristic, r));
 
             Console.WriteLine();
             Console.WriteLine("Partitions:");
             List<List<List<int>>> testBrute = GeneratePartitions(initialSet, r);
-            foreach(List<List<int>> l in testBrute)
+            foreach (List<List<int>> l in testBrute)
             {
                 PrintSolution(l);
                 Console.WriteLine();
             }
+        } 
 
+        public static void FileInput()
+        {
+            List<int> initialSet = new List<int>();
+            int r;
+            Console.WriteLine("Input file name:");
+            string fileName = Console.ReadLine();
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            if(lines.Length != 4)
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            else
+            {
+                if (!(int.TryParse(lines[0], out r)))
+                {
+                    Console.WriteLine("Invalid input!");
+                    return;
+                }
+                if (lines[3].Contains(','))
+                {
+                    string[] weights = lines[3].Split(',');
+                    foreach(string w in weights)
+                    {
+                        int number;
+                        if (int.TryParse(w, out number))
+                            initialSet.Add(number);
+                    }
+                }
+                else
+                {
+                    int size;
+                    if (int.TryParse(lines[3], out size))
+                    {
+                        Random rnd = new Random();
+                        for (int i=0; i< size; i++)
+                        {
+                            initialSet.Add(rnd.Next(size * 4));
+                        }
+                    }
+                }
+                if(lines[1] == "o")
+                {
+                    List<List<int>> resultBrute = new List<List<int>>();
+                    if (lines[2] == "1")
+                    {
+                        resultBrute = BruteForce(initialSet, r, Fitness);
+                        PrintSolution(resultBrute);
+                        Console.WriteLine("Initial fitness function value: {0}", Fitness(resultBrute, r));
+                    }
+                    else
+                    {
+                        resultBrute = BruteForce(initialSet, r, MyFitness);
+                        PrintSolution(resultBrute);
+                        Console.WriteLine("Second fitness function value: {0}", MyFitness(resultBrute, r));
+                    }
+                }
+                else if(lines[1] == "h")
+                {
+                    List<List<int>> resultHeuristic = Heuristic(initialSet, r);
+                    PrintSolution(resultHeuristic);
+                    if(lines[2] == "1")
+                        Console.WriteLine("Initial fitness function value: {0}", Fitness(resultHeuristic, r));
+                    else
+                        Console.WriteLine("Second fitness function value: {0}", MyFitness(resultHeuristic, r));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
         }
 
-        public static List<List<int>> BruteForce(List<int> initialSet, int r)
+        public static List<List<int>> BruteForce(List<int> initialSet, int r, Func<List<List<int>>, int, int> fitness)
         {
             List<int> initialCopy = new List<int>(initialSet);
             List<List<List<int>>> allSets = GeneratePartitions(initialCopy, r);
@@ -60,10 +157,10 @@ namespace AaCProject
             int tmp = initialSet.Sum();
             foreach (List<List<int>> list in allSets)
             {
-                if (Fitness(list) < tmp)
+                if (fitness(list, r) < tmp)
                 {
                     bestIndex = allSets.IndexOf(list);
-                    tmp = Fitness(list);
+                    tmp = fitness(list, r);
                 }
             }
             return allSets[bestIndex];
@@ -97,7 +194,8 @@ namespace AaCProject
             {
                 int s1 = initialSet.First();
                 initialSet.RemoveAt(0);
-                List<List<List<int>>> Sp = GeneratePartitions(initialSet, r);
+                List<int> initialCopy = new List<int>(initialSet);
+                List<List<List<int>>> Sp = GeneratePartitions(initialCopy, r);
                 foreach(List<List<int>> p in Sp)
                 {
                     foreach(List<int> sub in p)
@@ -112,7 +210,8 @@ namespace AaCProject
                         allSets.Add(pp);
                     }
                 }
-                List<List<List<int>>> Spp = GeneratePartitions(initialSet, r-1);
+                List<int> initialCopy2 = new List<int>(initialSet);
+                List<List<List<int>>> Spp = GeneratePartitions(initialCopy2, r-1);
                 foreach(List<List<int>> p in Spp)
                 {
                     List<int> pp = new List<int>();
@@ -156,7 +255,7 @@ namespace AaCProject
             return index;
         }
 
-        public static int Fitness(List<List<int>> list)
+        public static int Fitness(List<List<int>> list, int r)
         {
             List<int> weights = new List<int>();
             foreach(List<int> set in list)
